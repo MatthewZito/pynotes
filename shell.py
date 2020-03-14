fe610ccbb82d625a31730829aa3fe7c567b5045f
MatthewZito <matthewtzito@gmail.com>
2020-03-14 00:07:39 -0700

    pynotes works w/both args and gui

--- shell.py
+++ shell.py
@@ -1,56 +1,62 @@
 #! /usr/bin/env python3
 import argparse
 import os
-import os.path
-import json
-import os
-import sys
-import re
-import inquirer
 from os import listdir
-from pprint import pprint
 from os.path import isfile, join
-
-
+import json
+import inquirer
 
 ### Config Notes Dir ###
 
 # check if config file and path val therein exist
-path = '/home/goldmund/Documents/pynotes_dir'
-if (not os.path.exists(path)):
-    new_path = input('Set a path for your notes: ')
-    os.mkdir(f'{new_path}/pynotes_dir')
+# path = '/home/goldmund/Documents/pynotes_dir'
 
-    # set config file, needs to read path from config file
-    # with open('.pynotes_config.json') as json_data_file:
-    #     datas = json.load(json_data_file)
-    #     print(datas)
-    
-# Args Given
+
+# Runs If Args Given
 def run(args):
+    path_existence = False
+    path = ''
+    while (path_existence == False):
+        try:
+            with open('.pynotes_config.json', encoding='utf-8') as config_file:
+                data = json.load(config_file)
+                path = data['notes_storage_path']
+                path_existence = True
+        except:
+    # set config file, needs to read path from config file
+            data = {}
+            data['notes_storage_path'] = ""
+            new_path = input('Set a path for your notes: ')
+            data['notes_storage_path'] = f'{new_path}/pynotes_dir'
+            with open('.pynotes_config.json', 'w', encoding='utf-8') as outfile:
+                json.dump(data, outfile)
+            os.mkdir(f'{new_path}/pynotes_dir')
+
     if (args.new is not None):
-        with open(f'{path}/{args.new}.txt', mode='a') as new_file:
+        with open(f'{path}/{args.new}.txt', mode='a') as new_arg_file:
             writable = input('Write new note: ')
-            new_file.write('\n' + writable)
+            new_arg_file.write('\n' + writable)
     elif (args.read is not None):
-        with open(f'{path}/{args.read}.txt', mode='r') as read_file:
-            print(read_file.read())
+        with open(f'{path}/{args.read}.txt', mode='r') as read_arg_file:
+            print(read_arg_file.read())
     elif (args.edit is not None):
-        with open(f'{path}/{args.edit}.txt', mode='r') as edit_file:
-            print(edit_file.read())
-        with open(f'{path}/{args.edit}.txt', mode='a') as edit_file:
+        with open(f'{path}/{args.edit}.txt', mode='r') as edit_arg_file:
+            print(edit_arg_file.read())
+        with open(f'{path}/{args.edit}.txt', mode='a') as edit_arg_file:
             writable = input('Add to note: ')
-            edit_file.write('\n' + writable)
+            edit_arg_file.write('\n' + writable)
     else:
-        run_action_interface()
+        run_action_interface(path)
 
 ### Action Interface ### 
 
-def run_action_interface():
-    ### Functions - Actions ###
+def run_action_interface(path):
+### Format Paths ###
+    print(path)
     all_files = [f for f in listdir(path) if isfile(join(path, f))]
     stripped_files = list(map(lambda x: x.replace('.txt',''),all_files))
 
+### FUNCTIONS ###
 
     # New File
     def new_file():
@@ -74,7 +80,9 @@ def run_action_interface():
         with open(f'{path}/{edit_file_name}.txt', mode='a') as edit_file:
             writable = input('Add to note: ')
             edit_file.write('\n' + writable)
-    
+
+### Options UI ###
+
     options = [("Create a new note",'n'), ("Read an existing note", 'r'), ("Append to an existing note", "a")]
     action = inquirer.list_input("Select an option: ", choices=options)
     
@@ -84,6 +92,9 @@ def run_action_interface():
         read_file()
     elif (action == 'a'):
         edit_file()
+
+
+### MAIN ARGPARSE FUNC ###
 
 # TO-DO: add direct func calls if -r or -e options are passed w/out a file arg
 def main():
